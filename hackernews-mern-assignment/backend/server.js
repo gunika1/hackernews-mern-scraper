931324a2ec29d341519c80cf06e605f681e18294
oneaxissolutions <oneaxissolutions@email.com>
2026-05-07 16:14:49 +0530

    Fix CORS issue for deployed frontend

--- hackernews-mern-assignment/backend/server.js
+++ hackernews-mern-assignment/backend/server.js
@@ -13,7 +13,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://hackernews-mern-frontend.vercel.app",
+    ],
+    credentials: true,
   })
 );
 
